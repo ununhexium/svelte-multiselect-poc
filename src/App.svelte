1ea5d778aef9abe2cf1cc0cf687f910af2431855
Christophe '116' Loiseau <116@lab0.net>
2022-09-21 20:32:12 +0200

    Tooltip

--- src/App.svelte
+++ src/App.svelte
@@ -20,7 +20,7 @@
   let mouseDownState: boolean = false;
 
   const doMouseEnter = (i: number) => {
-    console.log("Enter", i)
+    // console.log("Enter", i)
     if (mouseDown) {
       let min = Math.min(i, mouseDownIndex);
       let max = Math.max(i, mouseDownIndex);
@@ -42,12 +42,12 @@
     fields[i].status = mouseDownState
     mouseDown = true;
     addEventListener('mouseup', doMouseUp)
-    console.log("Mouse down", mouseDown, "state", mouseDownState)
+    // console.log("Mouse down", mouseDown, "state", mouseDownState)
   }
 
   function doMouseUp() {
     mouseDown = false
-    console.log("Mouse down", mouseDown)
+    // console.log("Mouse down", mouseDown)
   }
 
   const doReset = () => {
@@ -84,11 +84,29 @@
     {#each fields as {name, status}, i}
       {#if status}
         <div on:mouseenter={() => doMouseEnter(i)} on:mousedown={() => doMouseDown(i)} class="on">
-          <p>☑{name}</p>
+          <p>☑ {name}</p>
         </div>
       {:else}
         <div on:mouseenter={() => doMouseEnter(i)} on:mousedown={() => doMouseDown(i)}>
-          <p>☐{name}</p>
+          <p>☐ {name}</p>
+        </div>
+      {/if}
+    {/each}
+  </fieldset>
+
+  <h2>Indicator <button on:click={doReset}>↺</button></h2>
+
+  <fieldset>
+    {#each fields as {name, status}, i}
+      {#if status}
+        <div class="tooltip on" on:mouseenter={() => doMouseEnter(i)} on:mousedown={() => doMouseDown(i)}>
+          <p >☑ {name}</p>
+          <span class="tooltiptext">Click and drag for multi select</span>
+        </div>
+      {:else}
+        <div class="tooltip" on:mouseenter={() => doMouseEnter(i)} on:mousedown={() => doMouseDown(i)}>
+          <p >☐ {name}</p>
+          <span class="tooltiptext">Click and drag for multi select</span>
         </div>
       {/if}
     {/each}
@@ -97,6 +115,10 @@
 </main>
 
 <style>
+  fieldset {
+    margin-bottom: 10px;
+  }
+
   fieldset > div {
     display: flex;
     user-select: none;
@@ -108,5 +130,52 @@
 
   .on {
     background-color: #ddf;
+  }
+
+  /* Tooltip container */
+  .tooltip {
+    position: relative;
+    /*display: inline-block;*/
+    /*border-bottom: 1px dotted black; !* If you want dots under the hoverable text *!*/
+  }
+
+  /* Tooltip text */
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+
+    /* Position the tooltip text */
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -60px;
+
+    /* Fade in tooltip */
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  /* Tooltip arrow */
+  .tooltip .tooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+  }
+
+  /* Show the tooltip text when you mouse over the tooltip container */
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
+    opacity: 1;
   }
 </style>
